13d3efdeb27963a1a3ab6fedc2aef5e1ef24a538
YAO Matrix <matrix.yao@intel.com>
2016-09-02 06:45:18 +0800

    complete environment setup scripts
    Signed-off-by: YAO Matrix <yaoweifeng0301@gmail.com>

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+
 # vim
 sudo apt-get install vim
 
@@ -6,18 +8,30 @@ sudo apt install subversion git
 
 # Android
 sudo apt-get install openjdk-8-jdk
-
-sudo apt-get install bison g++-multilib gperf libxml2-utils
+sudo apt-get install bison g++-multilib build-essential gperf libxml2-utils
+sudo apt-get install make cmake
 
 # Caffe
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install libleveldb-dev liblmdb-dev libhdf5-serial-dev
+sudo apt-get install libsnappy-dev
+sudo apt-get install libopencv-dev 
+sudo apt-get install -y libprotobuf-dev protobuf-compiler
 sudo apt-get install --no-install-recommends libboost-all-dev
-sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
+sudo apt-get install libgflags-dev libgoogle-glog-dev libgtest-dev
 sudo apt-get install libatlas-base-dev
-sudo apt-get install python-dev
+
+pushd /usr/src/gtest
+cmake .
+make
+sudo cp *a /usr/lib
+popd
 
 # python packages
-sudo apt-get install python-numpy
+sudo apt-get install python-dev python-pip libpython-dev
+sudo apt-get install python-numpy python-protobuf
+sudo apt-get install swig doxygen
+pip install 'sphinx>=1.4.0'
+pip install sphinx_rtd_theme breathe recommonmark
 
 # project management
 sudo apt-get install maven
