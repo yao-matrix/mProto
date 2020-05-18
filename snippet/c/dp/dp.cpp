cc6dd185cc20d1f7e040e06d77d2bb0dc490dea6
YAO Matrix <matrix.yao@intel.com>
2020-05-18 07:14:56 -0400

    xxx
    Signed-off-by: YAO Matrix <matrix.yao@intel.com>

--- snippet/c/dp/dp.cpp
+++ snippet/c/dp/dp.cpp
@@ -4,6 +4,10 @@
 #include <sys/time.h>
 #include <inttypes.h>
 #include <malloc.h> 
+
+#define TIME_DIFF(a,b) ((b.tv_sec - a.tv_sec) * 1000000 + (b.tv_usec - a.tv_usec))
+#define BS 4
+#define VECTOR_DIM 128
  
 static inline float horizontal_add_v256(__m256 v) {
     __m256 x1 = _mm256_hadd_ps(v, v);
@@ -16,39 +20,57 @@ static inline float horizontal_add_v256(__m256 v) {
 }
 
 
-void dotp_avx2(const float *lhs, const float *rhs, size_t size, float *result) {
+#if 1
+void dotp_avx2(const float *lhs, const float *rhs, size_t size, float *score) {
+    __m256 ymm_sum0 = _mm256_setzero_ps();
     __m256 ymm_sum1 = _mm256_setzero_ps();
-    __m256 ymm_sum2 = _mm256_setzero_ps();
 
-    for (int i = 0; i < size; i += 16) {
-        ymm_sum1 = _mm256_fmadd_ps(_mm256_loadu_ps(lhs + i), _mm256_loadu_ps(rhs + i), ymm_sum1);
-        ymm_sum2 = _mm256_fmadd_ps(_mm256_loadu_ps(lhs + i + 8), _mm256_loadu_ps(rhs + i + 8), ymm_sum2);
+
+    for (size_t i = 0; i < size; i += 16) {
+        ymm_sum0 = _mm256_fmadd_ps(_mm256_loadu_ps(lhs + i), _mm256_loadu_ps(rhs + i), ymm_sum0);
+        ymm_sum1 = _mm256_fmadd_ps(_mm256_loadu_ps(lhs + i + 8), _mm256_loadu_ps(rhs + i + 8), ymm_sum1);
     }
 
-    *result = horizontal_add_v256(_mm256_add_ps(ymm_sum1, ymm_sum2));
+    *score = horizontal_add_v256(_mm256_add_ps(ymm_sum0, ymm_sum1));
 
     return;
 }
+#else
+void dotp_avx2(const float *lhs, const float *rhs, size_t size, float *score) {
+    __m256 ymm_sum0 = _mm256_setzero_ps();
+    __m256 ymm_sum1 = _mm256_setzero_ps();
 
 
-void dotp4_avx2(const float *base, float *array[4], size_t offset, size_t size, float *result) {
-    __m256 ymm_sum_0[4] = {_mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps()};
-    __m256 ymm_sum_1[4] = {_mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps()};
+    for (size_t i = 0; i < size; i += 16) {
+        ymm_sum0 = _mm256_fmadd_ps(_mm256_castsi256_ps(_mm256_stream_load_si256((__m256i*)(lhs + i))), _mm256_loadu_ps(rhs + i), ymm_sum0);
+        ymm_sum1 = _mm256_fmadd_ps(_mm256_castsi256_ps(_mm256_stream_load_si256((__m256i*)(lhs + i + 8))), _mm256_loadu_ps(rhs + i + 8), ymm_sum1);
+    }
 
-    size_t index = offset / size;
+    *score = horizontal_add_v256(_mm256_add_ps(ymm_sum0, ymm_sum1));
 
-    for (int i = 0; i < size; i += 16) {
+    return;
+}
+#endif
+
+
+void dotp4_avx2(const float *base, const float *query, size_t offset, size_t size, float *score) {
+    __m256 ymm_sum_0[BS] = {_mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps()};
+    __m256 ymm_sum_1[BS] = {_mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps()};
+
+    // size_t index = offset / size;
+
+    for (size_t i = 0; i < size; i += 16) {
         __m256 v_base_0 = _mm256_loadu_ps(base + offset + i);
         __m256 v_base_1 = _mm256_loadu_ps(base + offset + i + 8);
 
-        for (int j = 0; j < 4; j++) {
-            ymm_sum_0[j] = _mm256_fmadd_ps(v_base_0, _mm256_loadu_ps(array[j] + offset + i), ymm_sum_0[j]);
-            ymm_sum_1[j] = _mm256_fmadd_ps(v_base_1, _mm256_loadu_ps(array[j] + offset + i + 8), ymm_sum_1[j]);
+        for (size_t j = 0; j < BS; j++) {
+            ymm_sum_0[j] = _mm256_fmadd_ps(v_base_0, _mm256_loadu_ps(query + j * size + i), ymm_sum_0[j]);
+            ymm_sum_1[j] = _mm256_fmadd_ps(v_base_1, _mm256_loadu_ps(query + j * size + i + 8), ymm_sum_1[j]);
         }
     }
 
-    for (int j = 0; j < 4; j++) {
-        result[j] = horizontal_add_v256(_mm256_add_ps(ymm_sum_0[j], ymm_sum_1[j]));
+    for (size_t j = 0; j < BS; j++) {
+        score[j] = horizontal_add_v256(_mm256_add_ps(ymm_sum_0[j], ymm_sum_1[j]));
     }
 
     return;
@@ -62,102 +84,171 @@ static inline float horizontal_add_v512(__m512 v) {
     return horizontal_add_v256(low) + horizontal_add_v256(high);
 }
 
-void dotp4_avx3(const float *base, float *array[4], size_t offset, size_t size, float **result) {
 
-    __m512 zmm_sum_0[4] = {_mm512_setzero_ps(), _mm512_setzero_ps(), _mm512_setzero_ps(), _mm512_setzero_ps()};
-    __m512 zmm_sum_1[4] = {_mm512_setzero_ps(), _mm512_setzero_ps(), _mm512_setzero_ps(), _mm512_setzero_ps()};
+void dotp_avx3(const float *lhs, const float *rhs, size_t size, float *score) {
 
-    size_t index = offset / size;
+    __m512 zmm_sum0 = _mm512_setzero_ps();
+    __m512 zmm_sum1 = _mm512_setzero_ps();
 
-    for (int i = 0; i < size; i += 32) {
+
+    for (size_t i = 0; i < size; i += 32) {
+        zmm_sum0 = _mm512_fmadd_ps(_mm512_loadu_ps(lhs + i), _mm512_loadu_ps(rhs + i), zmm_sum0);
+        zmm_sum1 = _mm512_fmadd_ps(_mm512_loadu_ps(lhs + i + 16), _mm512_loadu_ps(rhs + i + 16), zmm_sum1);
+    }
+
+    *score = horizontal_add_v512(_mm512_add_ps(zmm_sum0, zmm_sum1));
+
+    return;
+}
+
+
+void dotp4_avx3(const float *base, float *query, size_t offset, size_t size, float *score) {
+
+    __m512 zmm_sum_0[BS] = {_mm512_setzero_ps(), _mm512_setzero_ps(), _mm512_setzero_ps(), _mm512_setzero_ps()};
+    __m512 zmm_sum_1[BS] = {_mm512_setzero_ps(), _mm512_setzero_ps(), _mm512_setzero_ps(), _mm512_setzero_ps()};
+
+    // size_t index = offset / size;
+
+    for (size_t i = 0; i < size; i += 32) {
         __m512 v_base_0 = _mm512_loadu_ps(base + offset);
         __m512 v_base_1 = _mm512_loadu_ps(base + offset + 16);
 
-        for (int j = 0; j < 4; j++) {
-            zmm_sum_0[j] = _mm512_fmadd_ps(v_base_0, _mm512_loadu_ps(array[j] + offset + i), zmm_sum_0[j]);
-            zmm_sum_1[j] = _mm512_fmadd_ps(v_base_1, _mm512_loadu_ps(array[j] + offset + i + 16), zmm_sum_1[j]);
+        for (size_t j = 0; j < BS; j++) {
+            zmm_sum_0[j] = _mm512_fmadd_ps(v_base_0, _mm512_loadu_ps(query + j * size + i), zmm_sum_0[j]);
+            zmm_sum_1[j] = _mm512_fmadd_ps(v_base_1, _mm512_loadu_ps(query + j * size + i + 16), zmm_sum_1[j]);
         }
     }
 
-    for (int j = 0; j < 4; j++) {
-        result[j] = horizontal_add_v512(_mm512_add_ps(zmm_sum_0[j], zmm_sum_1[j]));
+    for (size_t j = 0; j < BS; j++) {
+        score[j] = horizontal_add_v512(_mm512_add_ps(zmm_sum_0[j], zmm_sum_1[j]));
     }
 
     return;
 }
 #endif
 
-#define TIME_DIFF(a,b) ((b.tv_sec - a.tv_sec) * 1000000 + (b.tv_usec - a.tv_usec))
-#define BS 4
-#define VECTOR_DIM 128
-
 int main(int argc, char *argv[]) {
-    size_t vec_num = 1024 * 1024 * 8;
+    size_t base_vec_num = 1024 * 1024 * 8; // 8M vectors
 
-    float  *array[BS] = {NULL};
-    float  result[BS] = {0};
-    float  *base_array = NULL;
+    float  *query    = NULL;
+    float  *base     = NULL;
+    float  score[BS] = {0};
+
     size_t align_size = 64;
+
+    size_t base_elms = base_vec_num * VECTOR_DIM;
 
     struct timeval start, end;
 
-    base_array = (float*)memalign(align_size, sizeof(float) * vec_num * VECTOR_DIM);
-    for (size_t i = 0; i < BS; i++) {
-        array[i] = (float*)memalign(align_size, sizeof(float) * vec_num * VECTOR_DIM);
-    }
-
-    size_t vec_elms = vec_num * VECTOR_DIM;
-
-
-    for (size_t i = 0; i < vec_elms; i++) {
-        base_array[i] = 1.0;
-    }
-
-     
-    for (size_t j = 0; j < BS; j++) {    
-        for (size_t i = 0; i < vec_elms; i++) {
-            array[j][i] = i;
-        }
-    }
+    base = (float*)memalign(align_size, sizeof(float) * base_elms);
+    query = (float*)memalign(align_size, sizeof(float) * VECTOR_DIM * BS);
 
 #if 1
-    gettimeofday(&start, NULL);
-    for (size_t i = 0; i < vec_elms; i += VECTOR_DIM) {
-        for (size_t j = 0; j < BS; j++) {
-            dotp_avx2(base_array + i, array[j] + i, VECTOR_DIM, &result[j]);
+    // fill buffers
+    for (size_t i = 0; i < base_elms; i++) {
+        base[i] = 1.0;
+    }
+
+    for (size_t i = 0; i < BS; i++) {
+        for (size_t j = 0; j < VECTOR_DIM; j++) {
+            query[i * VECTOR_DIM + j] = j;
         }
     }
-    gettimeofday(&end, NULL);
-    printf("[AVX2 baseline] result: %f, time(ms): %f\n", result[0], TIME_DIFF(start, end) / 1000.0);
-    fflush(stdout);
 #endif
 
 #if 1
+    // warm up
+    for (size_t i = 0; i < base_elms; i += VECTOR_DIM) {
+        for (size_t j = 0; j < BS; j++) {
+            dotp_avx2(base + i, query + j * VECTOR_DIM, VECTOR_DIM, &score[j]);
+        }
+    }
+
     gettimeofday(&start, NULL);
-    for (size_t i = 0; i < vec_elms; i += VECTOR_DIM) {
-        dotp4_avx2(base_array, array, i, VECTOR_DIM, result);
+    for (size_t iter = 0; iter < 10; iter++) {
+        for (size_t i = 0; i < base_elms; i += VECTOR_DIM) {
+            for (size_t j = 0; j < BS; j++) {
+                dotp_avx2(base + i, query + j * VECTOR_DIM, VECTOR_DIM, &score[j]);
+                // if (i + VECTOR_DIM < base_elms) {
+                // _mm_prefetch(base + i + VECTOR_DIM, _MM_HINT_NTA);
+                // }
+            }
+        }
+    }
+
+    gettimeofday(&end, NULL);
+    printf("[AVX2 baseline] result: %f, time(ms): %f\n", score[0], TIME_DIFF(start, end) / 1000.0);
+    fflush(stdout);
+#endif
+
+#if 0
+    // warm up
+    for (size_t i = 0; i < base_elms; i += VECTOR_DIM) {
+        dotp4_avx2(base, query, i, VECTOR_DIM, score);
+    }
+
+    gettimeofday(&start, NULL);
+    for (size_t iter = 0; iter < 10; iter++) {
+        for (size_t i = 0; i < base_elms; i += VECTOR_DIM) {
+            // if (i + VECTOR_DIM < base_elms) {
+            //    _mm_prefetch(base + i + VECTOR_DIM, _MM_HINT_T0);
+            // }
+            dotp4_avx2(base, query, i, VECTOR_DIM, score);
+        }
     }
     gettimeofday(&end, NULL);
-    printf("[AVX2 batch] result: %f, time(ms): %f\n", result[0], TIME_DIFF(start, end) / 1000.0);
+    printf("[AVX2 batch] result: %f, time(ms): %f\n", score[0], TIME_DIFF(start, end) / 1000.0);
     fflush(stdout);
 #endif
 
 #if defined(__AVX512F__)
-    gettimeofday(&start, NULL);
-    for (size_t i = 0; i < vec_elms; i += VECTOR_DIM) {
-        dotp4_avx3(base_array, array, i, VECTOR_DIM, result);
+
+#if 0
+    // warm up
+    for (size_t i = 0; i < base_elms; i += VECTOR_DIM) {
+        for (size_t j = 0; j < BS; j++) {
+            dotp_avx3(base + i, query + j * VECTOR_DIM, VECTOR_DIM, &score[j]);
+        }
     }
+
+    gettimeofday(&start, NULL);
+    for (size_t iter = 0; iter < 10; iter++) {
+        for (size_t i = 0; i < base_elms; i += VECTOR_DIM) {
+            for (size_t j = 0; j < BS; j++) {
+                dotp_avx3(base + i, query + j * VECTOR_DIM, VECTOR_DIM, &score[j]);
+            }
+        }
+    }
+
     gettimeofday(&end, NULL);
-    printf("[AVX3 batch] result: %f, time(ms): %f\n", result[0], TIME_DIFF(start, end) / 1000.0);
+    printf("[AVX3 baseline] result: %f, time(ms): %f\n", score[0], TIME_DIFF(start, end) / 1000.0);
     fflush(stdout);
 #endif
 
-    free(base_array);
-    base_array = NULL;
-    for (size_t i = 0; i < BS; i++) {
-        free(array[i]);
-        array[i] = NULL;
-    }		
+#if 0
+    // warm up
+    for (size_t i = 0; i < base_elms; i += VECTOR_DIM) {
+        dotp4_avx3(base, query, i, VECTOR_DIM, score);
+    }
+ 
+    gettimeofday(&start, NULL);
+    for (size_t iter = 0; iter < 10; iter++) {
+        for (size_t i = 0; i < base_elms; i += VECTOR_DIM) {
+            dotp4_avx3(base, query, i, VECTOR_DIM, score);
+        }
+    }
+    gettimeofday(&end, NULL);
+    printf("[AVX3 batch] result: %f, time(ms): %f\n", score[0], TIME_DIFF(start, end) / 1000.0);
+    fflush(stdout);
+#endif
+
+
+#endif
+
+    free(base);
+    base = NULL;
+    free(query);
+    query = NULL;		
 
     return 0;
 }
